7c91b2702bc903341c28a3244ad12835fe2b47ed
blue-beaker <yyy5566778899@126.com>
2024-08-25 20:48:59 +0800

    patch

--- src/main/java/io/bluebeaker/mtetweaks/mixin/MixinMassFabricator.java
+++ src/main/java/io/bluebeaker/mtetweaks/mixin/MixinMassFabricator.java
@@ -5,13 +5,16 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.At;
 
+import ic2.core.block.invslot.InvSlotOutput;
 import ic2.core.block.machine.tileentity.TileEntityMassFabricator;
+import ic2.core.item.type.MiscResourceType;
+import ic2.core.ref.ItemName;
 
 @Mixin(TileEntityMassFabricator.class)
 public class MixinMassFabricator {
     @ModifyVariable(remap = false, method = "updateEntityServer()V", at = @At(value = "LOAD"),ordinal = 0)
     private double ifScrapFull(double scrapConversion) {
-        if( this.consumedScrap >= REQUIRED_SCRAP){
+        if(this.consumedScrap >= REQUIRED_SCRAP && this.outputSlot.canAdd(ItemName.misc_resource.getItemStack(MiscResourceType.matter))){
             return Math.max(scrapConversion,0.001);
         }
         return scrapConversion;
@@ -20,4 +23,6 @@ public class MixinMassFabricator {
     int consumedScrap;
     @Shadow(remap = false)
     static int REQUIRED_SCRAP;
+    @Shadow(remap = false)
+    InvSlotOutput outputSlot;
 }
