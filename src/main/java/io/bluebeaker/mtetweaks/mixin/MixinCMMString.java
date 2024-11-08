974d9908bf9a557aeb52d7adff0f5f5209cccead
blue-beaker <yyy5566778899@126.com>
2024-11-08 18:16:07 +0800

    use inject to replace return value

--- src/main/java/io/bluebeaker/mtetweaks/mixin/MixinCMMString.java
+++ src/main/java/io/bluebeaker/mtetweaks/mixin/MixinCMMString.java
@@ -2,20 +2,21 @@ package io.bluebeaker.mtetweaks.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.bluebeaker.mtetweaks.StartupTimer;
 import lumien.custommainmenu.lib.StringReplacer;
-import zone.rong.mixinextras.injector.ModifyReturnValue;
 
 @Mixin(StringReplacer.class)
 public abstract class MixinCMMString {
-    @ModifyReturnValue(remap = false,at = @At("RETURN"),method = "replacePlaceholders(Ljava/lang/String;)Ljava/lang/String;")
-    private static String replaceAdditionalPlaceholders(String source){
-        if(source.contains("#startuptime#")){
+    @Inject(remap = false,at = @At("RETURN"),method = "replacePlaceholders(Ljava/lang/String;)Ljava/lang/String;",cancellable = true)
+    private static void replaceAdditionalPlaceholders(String source,CallbackInfoReturnable<String> cir){
+        String original=cir.getReturnValue();
+        if(original.contains("#startuptime#")){
             long minutes = StartupTimer.startupTime / 1000L / 60L;
             long seconds = StartupTimer.startupTime / 1000L % 60L;
-            return source.replace("#startuptime#", minutes+"m "+seconds+"s");
+            cir.setReturnValue(original.replace("#startuptime#", minutes+"m "+seconds+"s"));
         }
-        return source;
     }
 }
